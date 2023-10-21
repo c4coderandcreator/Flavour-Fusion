60399c2fe498dc6110a2a7aaeb8bda5ebdc76a90
Aneena John Xavier <aneena_john_xavier@epam.com>
2023-10-21 08:23:11 +0530

    fix/issue-146: the menu styles on smaller screen is updated to fill the entire screen and disabled the scroll while menu is open
    Signed-off-by: Aneena John Xavier <aneena_john_xavier@epam.com>

--- javascript/landing.js
+++ javascript/landing.js
@@ -1,18 +1,31 @@
 /*====================Landing Page and Navbar Code by Navuuu====================*/
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+    nav = document.getElementById(navId);
+    console.log({toggleId, navId});
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show-menu')
+            nav.classList.toggle('show-menu');
+            const body = document.body;
+            if (nav.classList.contains('show-menu')) {
+                body.style.overflow = 'hidden';
+            } else {
+                body.style.overflow = 'visible';
+            }
+            // body.classList.toggle('hide-overflow');
+            // document.body.style.overflowY = 'hidden';
         })
     }
 }
 showMenu('nav-toggle','nav-menu')
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav__link');
+console.log(navLink);
 function linkAction(){
+    console.log("linkAction called")
     const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+    console.log("linkAction called: ", navMenu);
+    navMenu.classList.remove('show-menu');
+    document.body.style.overflow = 'visible';
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 const sections = document.querySelectorAll('section[id]')
