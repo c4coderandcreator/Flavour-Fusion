e498b5ac0d898478ea4700e394e055047e5eed8a
Coder And Creator <70572678+c4coderandcreator@users.noreply.github.com>
2023-10-26 11:50:07 +0530

    Revert "Added Testimonials Section"

--- javascript/landing.js
+++ javascript/landing.js
@@ -34,17 +34,3 @@ function scrollTop(){
 }
 window.addEventListener('scroll', scrollTop)
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    grabCursor: true,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-  
